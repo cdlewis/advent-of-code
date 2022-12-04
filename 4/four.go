d433c40627c0819f8e43c3804b9e3c8905b9e7b0
Chris Lewis <chris.lewis2@gmail.com>
2022-12-04 16:24:51 +1100

    day 4

---
+++ 4/four.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parsePairs(input string) [][]int {
+	splitInput := strings.FieldsFunc(input, func(r rune) bool { return r == '-' || r == ',' })
+
+	numbers := []int{}
+	for _, n := range splitInput {
+		result, err := strconv.ParseInt(n, 10, 64)
+
+		if err != nil {
+			panic(err)
+		}
+
+		numbers = append(numbers, int(result))
+	}
+
+	return [][]int{numbers[:2], numbers[2:]}
+}
+
+func overlaps(one []int, two []int) bool {
+	return (one[0] >= two[0] && one[0] <= two[1]) || (one[1] >= two[0] && one[1] <= two[1])
+}
+
+func main() {
+	dat, _ := os.ReadFile("./input")
+	raw := strings.Split(string(dat), "\n")
+
+	result := 0
+
+	for _, data := range raw {
+		pairs := parsePairs(data)
+		if overlaps(pairs[0], pairs[1]) || overlaps(pairs[1], pairs[0]) {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+
+	if result != 905 {
+		panic("unexpected result")
+	}
+}
