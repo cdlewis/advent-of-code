901f183570d952bd8f82527d4b8d1736f2524c8c
Chris Lewis <chris.lewis2@gmail.com>
2022-12-05 16:33:01 +1100

    day 5

--- 5/five.go
+++ 5/five.go
@@ -10,8 +10,35 @@ import (
 
 func main() {
 	dat, _ := os.ReadFile("./input")
-	raw := strings.Split(string(dat), "\n")
-	util.Flatten([][]int{})
+	instructions := strings.Split(string(dat), "\n")
+	stacks := util.Map([]string{
+		"ZJG",
+		"QLRPWFVC",
+		"FPMCLGR",
+		"LFBWPHM",
+		"GCFSVQ",
+		"WHJZMQTL",
+		"HFSBV",
+		"FJZS",
+		"MCDPFHBT",
+	}, func(i string) []byte { return []byte(i) })
 
-	fmt.Println(raw)
+	for _, serializedInstruction := range instructions {
+		tokens := strings.Split(serializedInstruction, " ")
+		count := util.ToInt(tokens[1])
+		from := util.ToInt(tokens[3]) - 1
+		to := util.ToInt(tokens[5]) - 1
+
+		curr := stacks[from][len(stacks[from])-count:]
+		stacks[from] = stacks[from][:len(stacks[from])-count]
+		stacks[to] = append(stacks[to], curr...)
+	}
+
+	result := string(util.Map(stacks, func(s []byte) byte { return s[len(s)-1] }))
+
+	fmt.Println(result)
+
+	if result != "GSLCMFBRP" {
+		panic("Unexpected result")
+	}
 }
