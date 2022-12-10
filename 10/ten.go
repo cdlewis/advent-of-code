afdaccdc9f7344643c25d047277b14217aa42d46
Chris Lewis <me@chrislewis.au>
2022-12-10 18:36:28 +1100

    minor tweaks to day 10

--- 10/ten.go
+++ 10/ten.go
@@ -12,7 +12,8 @@ func main() {
 	serialisedInstructions := strings.Split(util.GetInput(10, false, ""), "\n")
 
 	cycle := 0
-	x := 1 // cursor position
+	x := 1  // cursor position
+	r1 := 0 // register 1
 
 	// Clear the screen and create some room
 	fmt.Print("\033[2J")
@@ -21,17 +22,13 @@ func main() {
 	for len(serialisedInstructions) > 0 {
 		// Move cursor to the top of the screen and write our summary for this cycle
 		fmt.Printf("\033[%d;%dH", 0, 0)
-		fmt.Print("\u001b[1000D")
 		fmt.Println("Cycle:       ", cycle)
-		fmt.Print("\u001b[1000D")
-		fmt.Println("Instruction: ", serialisedInstructions[0])
+		fmt.Println("Instruction: ", serialisedInstructions[0], "               ")
 
 		currentRow := 0
 		currentColumn := cycle
-		if cycle/40 > 0 { // avoid div by 0 issues
-			currentRow = cycle / 40
-			currentColumn = cycle % (currentRow * 40)
-		}
+		currentRow = cycle / 40
+		currentColumn = cycle % (util.Max(currentRow, 1) * 40)
 
 		// Move cursor to the current row/column (with some spacing to separate from the summary)
 		fmt.Printf("\033[%d;%dH", currentRow+5, currentColumn)
@@ -44,13 +41,15 @@ func main() {
 		serialisedInstructions = serialisedInstructions[1:]
 
 		if t[0] == "addx" {
-			// Invent a new instruction to simulate delaying execution addx
-			serialisedInstructions = append([]string{"do_addx " + t[1]}, serialisedInstructions...)
-		} else if t[0] == "do_addx" {
-			x += util.ToInt(t[1])
+			// Invent a new instruction (add register r1 to counter) to simulate delaying execution addx
+			serialisedInstructions = append([]string{"add_r1"}, serialisedInstructions...)
+			r1 = util.ToInt(t[1])
+		} else if t[0] == "add_r1" {
+			x += r1
 		}
 
 		cycle++
-		time.Sleep(100 * time.Millisecond)
+
+		time.Sleep(10 * time.Millisecond)
 	}
 }
