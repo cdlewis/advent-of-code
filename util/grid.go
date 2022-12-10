3ee08ea17573650ae41739decabce7a736e08113
Chris <chris@Chriss-MacBook-Air.local>
2022-12-10 15:42:13 +1100

    add directions helper

--- util/grid.go
+++ util/grid.go
@@ -3,3 +3,5 @@ package util
 func ValidCoordinate[U any](i int, j int, grid [][]U) bool {
 	return i >= 0 && j >= 0 && i < len(grid) && j < len(grid[0])
 }
+
+var Directions = [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
