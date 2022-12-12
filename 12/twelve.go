1bd7cae39b5456f8dce56eb18181370d75fb5676
Chris Lewis <me@chrislewis.au>
2022-12-12 18:45:34 +1100

    minor tweaks

--- 12/twelve.go
+++ 12/twelve.go
@@ -10,38 +10,31 @@ func Twelve(useTest bool, testInput string) int {
 	graph := util.Map(strings.Split(util.GetInput(12, useTest, testInput), "\n"), func(s string) []byte { return []byte(s) })
 
 	end := [2]int{}
-	for i := range graph {
-		for j := range graph[i] {
-			if graph[i][j] == 'E' {
-				end = [2]int{i, j}
+	for idx := range graph {
+		for jdx, cell := range graph[i] {
+			if cell == 'E' {
+				end = [2]int{idx, jdx}
 			}
 		}
 	}
 
-	bestResult := -1
-
-	shortestPath, exists := util.ShortestUnweightedPath(
+	shortestPath, _ := util.ShortestUnweightedPath(
 		graph,
 		end,
 		func(x [2]int) bool { return graph[x[0]][x[1]] == 'a' || graph[x[0]][x[1]] == 'S' },
 		func(x, y [2]int) bool { return normalize(graph[x[0]][x[1]]) <= normalize(graph[y[0]][y[1]])+1 },
 	)
 
-	if exists && (shortestPath < bestResult || bestResult == -1) {
-		bestResult = shortestPath
-	}
-
-	return bestResult
+	return shortestPath
 }
 
 func normalize(b byte) int {
-	if b == 'S' {
+	switch b {
+	case 'S':
 		return int('a')
-	}
-
-	if b == 'E' {
+	case 'E':
 		return int('z')
+	default:
+		return int(b)
 	}
-
-	return int(b)
 }
