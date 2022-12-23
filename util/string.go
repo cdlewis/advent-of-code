29d420bd62b9d837845d729128b19fcf9a08a020
Chris Lewis <me@chrislewis.au>
2022-12-23 15:51:35 +1100

    better string padding

--- util/string.go
+++ util/string.go
@@ -1,6 +1,9 @@
 package util
 
-import "strings"
+import (
+	"bytes"
+	"strings"
+)
 
 // SplitStringOn is like strings.Split but takes in a slice of strings that are
 // all used as dividers in the incoming string
@@ -20,11 +23,27 @@ func SplitStringOn(in string, cutset []string) []string {
 	return parts
 }
 
-func PadRight(str string, lenght int, pad string) string {
-	for {
-		str += pad
-		if len(str) > lenght {
-			return str[0:lenght]
-		}
+// PadRight returns a new string of a specified length in which the end of the current string is padded with spaces or with a specified Unicode character.
+func PadRight(str string, length int, pad byte) string {
+	if len(str) >= length {
+		return str
 	}
+	buf := bytes.NewBufferString(str)
+	for i := 0; i < length-len(str); i++ {
+		buf.WriteByte(pad)
+	}
+	return buf.String()
+}
+
+// PadLeft returns a new string of a specified length in which the beginning of the current string is padded with spaces or with a specified Unicode character.
+func PadLeft(str string, length int, pad byte) string {
+	if len(str) >= length {
+		return str
+	}
+	var buf bytes.Buffer
+	for i := 0; i < length-len(str); i++ {
+		buf.WriteByte(pad)
+	}
+	buf.WriteString(str)
+	return buf.String()
 }
