61a93cd44d0ae6171243cbee8092e4400c95c4d7
Chris Lewis <me@chrislewis.au>
2022-12-23 15:47:45 +1100

    scaffold remaining days

---
+++ 24/twenty_four.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cdlewis/advent-of-code/util"
+)
+
+func main() {
+	raw := util.GetInput(24, false, "")
+	fmt.Println(raw)
+}
---
+++ 25/twenty_five.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cdlewis/advent-of-code/util"
+)
+
+func main() {
+	raw := util.GetInput(25, false, "")
+	fmt.Println(raw)
+}
