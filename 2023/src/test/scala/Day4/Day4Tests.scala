4957298af0a90e392250dc0c731d50d6f884b3ac
Chris Lewis <me@chrislewis.au>
2025-01-15 21:18:30 -0800

    move to 2023

---
+++ 2023/src/test/scala/Day4/Day4Tests.scala
@@ -0,0 +1,11 @@
+package Day4
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day4Tests extends AnyFunSuite {
+
+    test("Day4 runs as expected") {
+        assert(Day4.solve() == 23806951)
+    }
+
+}
